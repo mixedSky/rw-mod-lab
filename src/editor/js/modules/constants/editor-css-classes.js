3deb255d7d0b775a08ba527bb90a8637f620639a
mixedSky <mixedsky125@gmail.com>
2026-01-13 12:12:59 +0300

    added folder 'constants' in modules and js file 'editor-css-classes'

---
+++ src/editor/js/modules/constants/editor-css-classes.js
@@ -0,0 +1,16 @@
+export const CSS = Object.freeze({
+    /* APP STYLE */
+    APP: "app",
+    /* LOADING STYLE */
+    LOADING_SCREEN_BACKGROUND: "overlay",
+    /* TYPOGRAPHY */
+    /* Fonts */ 
+    ALUMNI_SANS_PINSTRIPE_REGULAR: "alumni-sans-pinstripe-regular",
+    ALUMNI_SANS_PINSTRIPE_BOLD: "alumni-sans-pinstripe-bold",
+    /* Fonts size */
+    TEXT_TINY: "text-tiny",
+    TEXT_SMALL: "text-small",
+    TEXT_NORMAL: "text-normal",
+    TEXT_LARGE: "text-large",
+    TEXT_GIANT: "text-giant",
+});
