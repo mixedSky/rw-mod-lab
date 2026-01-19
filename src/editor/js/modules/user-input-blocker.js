2aa85c2b170d3dc349bf45705cc0f3069db96b32
mixedSky <mixedsky125@gmail.com>
2026-01-19 19:20:37 +0300

    The key-events-blocker.js file was renamed to user-input-blocker.js and its functionality was changed.

---
+++ src/editor/js/modules/user-input-blocker.js
@@ -0,0 +1,48 @@
+export class KeyEventsBlocker {
+
+    static #isKeyboardBlocked = false;
+
+    static #eventList = ["keydown", "keypress", "keyup"];
+    static #handler = e => {
+        e.preventDefault();
+        e.stopPropagation();
+    };
+
+    static get isKeyboardBlocked() { return this.#isKeyboardBlocked; }
+
+    static #addHandlers() {
+        this.#eventList.forEach(event =>
+            document.addEventListener(event, this.#handler, true)
+        );
+    }
+
+    static #removeHandlers() {
+        this.#eventList.forEach(event =>
+            document.removeEventListener(event, this.#handler, true)
+        );
+    }
+
+    static blockKeyboard() {
+
+        if (this.#isKeyboardBlocked) {
+            console.error(new Error("Calling for blocking user input when it is already blocked"));
+            debugger;
+            return;
+        }
+
+        this.#addHandlers();
+        this.#isKeyboardBlocked = true;
+    }
+
+    static unblockKeyboard() {
+        if (this.#isKeyboardBlocked === false) {
+            console.error(new Error("Call to unlock user input when it is not locked"));
+            debugger;
+            return;
+        }
+
+        this.#removeHandlers();
+        this.#isKeyboardBlocked = false;
+    }
+}
+
