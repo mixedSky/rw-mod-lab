deec64f5efb7d8992272b5eee4ac776e31d8aba5
mixedSky <mixedsky125@gmail.com>
2026-01-28 18:39:45 +0300

    Old text localization system files have been removed.

--- src/editor/js/modules/localization.js
+++
@@ -1,8 +0,0 @@
-export class Localization {
-  static getUserLanguage() {
-    const userLang = navigator.language || navigator.userLanguage;
-    const shortLang = userLang.split('-')[0];
-
-    return shortLang;
-  }
-}
